2512af7011fed623639ca184d80b44f2e9cc2f42
김경현 <@DESKTOP-JFJ271B>
2019-03-27 10:31:44 +0900

    0.13

---
+++ GITGraphic/Shape.java
@@ -0,0 +1,23 @@
+package Shape;
+
+import java.awt.Graphics;
+
+public abstract class Shape {
+	protected int x1 ,y1, x2, y2;
+	
+	public void setOrigin(int x, int y ){//원점 세팅
+		this.x1 = x;
+		this.y1 = y;
+		this.x2 = x;
+		this.y2 = y;
+	}
+	public void setPoint(int x, int y ){//원점 세팅
+		this.x2 = x;
+		this.y2 = y;
+	}
+	public void addPoint(int x, int y ){//원점 세팅
+	
+	}
+	abstract public void draw(Graphics graphics);//좌표와 그림 그리는것 분리
+	
+}
